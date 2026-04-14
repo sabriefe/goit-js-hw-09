1870665f4f44615da41841c8f154696f9f1d8f7f
sabriefe <138400231+sabriefe@users.noreply.github.com>
2026-04-14 13:10:30 +0000

    Deploying to gh-pages from @ sabriefe/goit-js-hw-09@95ec073e765714a7f610e21fdbd49111bd62a182 🚀

---
+++ 01-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import"./assets/styles-l0sNRNKZ.js";const t=document.querySelector("h1"),e=document.createElement("p");e.textContent="deneme yazisi";t.insertAdjacentElement("afterend",e);
+//# sourceMappingURL=01-gallery.js.map
